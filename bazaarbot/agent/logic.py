1ca79c481d2861f5ab0147a98dd8df76bf8357d7
johnwyss <wyss.john@gmail.com>
2015-11-13 12:44:24 -0600

    Update logic.py

--- bazaarbot/agent/logic.py
+++ bazaarbot/agent/logic.py
@@ -1,56 +1,26 @@
 #! /usr/bin/env python
 
-package bazaarbot.agent;
-import haxe.io.Path;
-import haxe.Json;
-import hscript.Interp;
-import hscript.Parser;
-import openfl.Assets;
 
-/**
- * ...
- * @author 
- */
-class Logic
-{
-	private var init:Bool = false;
+import json
+import random
+
+class Logic(object):
+    def __init__(self, data=None):
+        self.init = False
+        # no implementation -- provide your own in a subclass
+    
+    # perform this logic on the given agent
+    def perform(self, agent, market):
+        """agent:BasicAgent object, market:Market object"""
+        pass #no implemenation -- provide your own in a subclass
 	
-	public function new(?data:Dynamic) 
-	{
-		//no implemenation -- provide your own in a subclass
-	}
+	def produce(self, agent, commodity, amount, chance=1.0):
+	    if (chance >= 1.0) or (random.random() < chance):
+	        agent.change_inventory(commodity, amount)
 	
-	/**
-	 * Perform this logic on the given agent
-	 * @param	agent
-	 */
-	
-	public function perform(agent:BasicAgent, market:Market):Void
-	{
-		//no implemenation -- provide your own in a subclass
-	}
-	
-	private function _produce(agent:BasicAgent, commodity:String, amount:Float, chance:Float = 1.0):Void
-	{
-		if (chance >= 1.0 || Math.random() < chance)
-		{
-			agent.changeInventory(commodity, amount);
-		}
-	}
-	
-	private function _consume(agent:BasicAgent, commodity:String, amount:Float, chance:Float = 1.0):Void
-	{
-		if (chance >= 1.0 || Math.random() < chance)
-		{
-			if (commodity == "money")
-			{
-				agent.money -= amount;
-			}
-			else
-			{
-				agent.changeInventory(commodity, -amount);
-			}
-		}
-	}
-	
-}
+    def consume(self, agent, commodity, amount, chance = 1.0):
+        if (chance >= 1.0) or (random.random() < chance):
+            if commodity == "money":
+                agent.money -= amount
+            else:
+                agent.change_inventory(commodity, -amount) # if money was a commodity this would not be needed
